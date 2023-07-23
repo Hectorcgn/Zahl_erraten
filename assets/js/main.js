41eccd0c8f07464e4eabbf58e0409a8775176c82
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-07-23 18:46:25 +0200

    update data (#1)
    finish core function
    finish restart button
    finish output field

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,102 @@
-
 "use strict";
 
 //inputs
 const inputField = document.querySelector('#user-input');
-console.log(inputField);
-const guessButton = document.body.querySelector('button')
-console.log(guessButton);
+
+const guessButton = document.body.querySelector('#guess-button')
+
+const roundselection = document.body.querySelectorAll('[name="rounds"]');
+const roundenauswahlSection = document.body.querySelector('.round-selection')
+const spielverlauf = document.body.querySelector('.spielverlauf');
+
 //outputs
 const outputField = document.querySelector('.output-section')
-//counter
-let counter = 0
 
+// variable für die runden anzahl
+let maxRunden = 0;
+let rundenZähler = 0;
+
+
+// function zum Begrenzen der Rundenanzahl
+roundselection.forEach(rounds => {
+    rounds.addEventListener('click', () => {
+        maxRunden = Number(rounds.id);
+        console.log('die ausgewählte Rundenzahl ist:', maxRunden)
+    });
+});
 //funtionen
-
-//funtion für die computer auswahl
-function computerTake() {
-    const randomNumber = 9
-    console.log('die nummer in der funtion ist:',randomNumber);
-    return randomNumber;
-}
-
-function spielstart(randomNumber) {
-    if (counter < 1){
-        randomNumber = computerTake()
+// Arrow-Funktion für die Computer-Auswahl
+    const computerTake = () => {
+        const randomNumber = Math.floor(Math.random() * 100);
+        return randomNumber;
     }
-    counter++;
-    console.log( 'die runden anzahl ist:',counter);
-    
-    const userTake = Number(inputField.value);
-    const userNummer = userTake;
-    console.log('der user hat gewählt:', userNummer);
-    const computerNumber = randomNumber
-    console.log('der Computer hat gewählt:', computerNumber);
-    
-    
-    if (userNummer === computerNumber) {
-        outputField.textContent = 'du hast es erraten';
-    } if (userNummer < computerNumber){
-        outputField.textContent = 'höher!';
-    } if (userNummer > computerNumber) {
-        outputField.textContent = 'tiefer!';
+let computerNummer = computerTake();
+
+// Arrow-Funktion für die Benutzer-Auswahl
+    const userTake = () => {
+        const userNummer = Number(inputField.value);
+        return userNummer
+    }
+    //Haupspiel funtion
+    const spielstart = () =>{
+        // zählen der versuche
+        let userNummer = userTake();
+        // Vergleichehn der Zahlen
+        if (maxRunden === 0){
+            outputField.innerHTML = `<p>lege eine Runden zahl fest</p>`
+            return
+        } 
+        if (userNummer === 0) {
+            outputField.innerHTML = `<p>Bitte gib eine Zahl ein</p>`
+            return
+        } 
+        rundenZähler++;
+        spielverlauf.innerHTML =`<p>${rundenZähler} / ${maxRunden}</p>`;
+        if(rundenZähler === 1){
+            roundenauswahlSection.classList.add('unsichtbar')
+            spielverlauf.classList.remove('unsichtbar')
+        }
+        if (userNummer === computerNummer) {
+        outputField.textContent = `dein tipp: ${userNummer} ist richtig du hast es erraten`;
+        guessButton.removeEventListener('click', spielstart);
+        guessButton.textContent = 'Neustart';
+        guessButton.addEventListener('click', neustartSpiel);
+        return
+        } else if (userNummer < computerNummer) {
+            outputField.innerHTML = `<p>versuch ${rundenZähler} dein tipp ist: ${userNummer}</p><p>versuch es höher</p>`;
+        } else if (userNummer > computerNummer) {
+            outputField.innerHTML = `<p>versuch ${rundenZähler} dein tipp ist: ${userNummer}</p><p>versuch es tiefer</p>`;
+        } 
+    console.log('die com nummer im spiel ist:', computerNummer);
+    console.log('Zahl die eingegben worden ist: ' ,userNummer);
+    // if Funktion für die Runden begrenzung
+    if (rundenZähler === maxRunden) {
+        outputField.innerHTML = `<p>versuch ${rundenZähler} schade die zahl war: ${computerNummer}</p><p>vielleicht beim nächsten mal</p>`;
+        guessButton.removeEventListener('click', spielstart);
+        guessButton.textContent = 'Neustart';
+        guessButton.addEventListener('click', neustartSpiel);
+        return
     }
 }
+//funtion zum zurück setzen des spiels
+const neustartSpiel = () => {
+    maxRunden = 0;
+    rundenZähler = 0
+    computerNummer = computerTake()
+    guessButton.textContent = 'guess';
+    inputField.value=''
+    outputField.textContent =''
+    roundenauswahlSection.classList.remove('unsichtbar')
+    spielverlauf.classList.add('unsichtbar')
+    guessButton.addEventListener('click', spielstart);
+    guessButton.removeEventListener('click',neustartSpiel)
+    roundselection.forEach(rounds => {
+        rounds.checked = false;
+    });
 
+}
 
 //eventlistener
-guessButton.addEventListener('click',spielstart)
+guessButton.addEventListener('click',spielstart);
 
-
+//Custom-wert per extra input-feld hinzufügen
